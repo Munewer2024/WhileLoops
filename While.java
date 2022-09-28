37dfb081122c520c9aae8e69cd072b2e282b4fe1
Munewer2024 <112985215+Munewer2024@users.noreply.github.com>
2022-09-28 13:39:21 -0700

    Add files via upload

--- While.java
+++ While.java
@@ -1,10 +1,3 @@
- /**
-/**
- * Write a description of class If here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class While
 {
     int a = 5;
@@ -14,22 +7,24 @@ public class While
     
     public void run() {
        int i = 0;
-       while (i < 15) {
+       do {
            System.out.println("i is " + i + " but is less than 15");
            i += 1;
-       }
-       int k = 10;
-       while (k > 5) {
+       } while (i < 15);
+       
+       int k = 10;       
+       do {
            System.out.println("k is " + k + " but is greater than 5");
            k -= 1;
-       }
+       } while (k > 5);
+       
        int a = 5;
        int b = 15;
-       while (a < 10 && b <= 20) {
+       do {
            System.out.println("a is " + a + " but is less than 10 and b " + b + " but is less than or equal to 20");
            a += 1;
            b += 1;
-       }
+       } while (a < 10 && b <= 20);
     }
     
     public static void main(String[] args) {
